d55450a7f8390a2c92be1c85dd868847eea16b81
David Newswanger <newswangerd@berea.edu>
2015-11-06 08:50:35 -0500

    added firelds to landowner form

--- models/landowner_model.php
+++ models/landowner_model.php
@@ -5,50 +5,75 @@ include "model_form.php"
 class LandownerForm extends Form {
 
 	function __construct(){
-		$this->table_name="farmer";
-		$this->id_name="farmer_id"
+		$this->table_name="landowner";
+		$this->id_name="landowner_id"
 
 
-		// NEED TO CHANGE SOME OF THESE TO TEXT FIELDS
-		// NEED TO ADD FIRST NAME ETC.
-		$this->fields['to_rent'] = new CheckBox;
-		$this->fields['to_sell'] = new CheckBox;
-		$this->fields['to_intern'] = new CheckBox;
-		$this->fields['to_other'] = new CheckBox;
-		$this->fields['terms_other'] = new CheckBox;
-		$this->fields['pasture'] = new CheckBox;
-		$this->fields['tillable'] = new CheckBox;
-		$this->fields['organic'] = new CheckBox;
-		$this->fields['housing'] = new CheckBox;
-		$this->fields['describe_housing'] = new CheckBox;
-		$this->fields['infrastructure_storage'] = new CheckBox;
-		$this->fields['infrastructure_barn'] = new CheckBox;
-		$this->fields['infrastructure_greenhouse'] = new CheckBox;
-		$this->fields['infrastructure_goats'] = new CheckBox;
-		$this->fields['infrastructure_sheep'] = new CheckBox;
-		$this->fields['infrastructure_horses'] = new CheckBox;
-		$this->fields['equipment'] = new CheckBox;
-		$this->fields['equipment_other'] = new CheckBox;
-		$this->fields['irrigation'] = new CheckBox;
-		$this->fields['horticulture'] = new CheckBox;
-		$this->fields['livestock_cattle_beef'] = new CheckBox;
-		$this->fields['livestock_cattle_poultry'] = new CheckBox;
-		$this->fields['livestock_hogs'] = new CheckBox;
-		$this->fields['livestock_goats'] = new CheckBox;
-		$this->fields['livestock_sheep'] = new CheckBox;
-		$this->fields['livestock_horses'] = new CheckBox;
-		$this->fields['aquaculture'] = new CheckBox;
-		$this->fields['tobacco'] = new CheckBox;
-		$this->fields['lease_agreement'] = new CheckBox;
-		$this->fields['purchase_agreement'] = new CheckBox;
-		$this->fields['some_college'] = new CheckBox;
-		$this->fields['college_graduate'] = new CheckBox;
-		$this->fields['other_education'] = new CheckBox;
-		$this->fields['northern'] = new CheckBox;
-		$this->fields['eastern'] = new CheckBox;
-		$this->fields['goals'] = new CheckBox;
-		$this->fields['currently_farming'] = new CheckBox;
-		$this->fields['sell_produce'] = new CheckBox;
+		// General Information
+		$this->fields['fist_name'] = new TextField();
+		$this->fields['last_name'] = new TextField();
+		$this->fields['password'] = new TextField();
+		$this->fields['email'] = new TextField();
+		$this->fields['phone'] = new TextField();
+		$this->fields['address'] = new TextField();
+
+		// Terms
+		$this->fields['to_rent'] = new CheckBox();
+		$this->fields['to_sell'] = new CheckBox();
+		$this->fields['to_intern'] = new CheckBox();
+		$this->fields['to_other'] = new CheckBox();
+		$this->fields['terms_other'] = new TextField();
+
+		// Locattion
+		$this->fields['street'] = new TextField();
+		$this->fields['city'] = new TextField();
+		$this->fields['zip'] = new IntegerField();
+
+		// Acreage
+		$this->fields['acres_total'] = new IntegerField();
+		$this->fields['pasture_acres'] = new IntegerField();
+		$this->fields['tillable_acres'] = new IntegerField();
+		$this->fields['woodland_acres'] = new IntegerField();
+
+		//$this->fields['organic'] = new CheckBox;
+
+		// Housing
+		$this->fields['housing'] = new CheckBox();
+		$this->fields['describe_housing'] = new TextField();
+
+		// Infrastructure
+		$this->fields['infrastructure_storage'] = new CheckBox();
+		$this->fields['infrastructure_barn'] = new CheckBox();
+		$this->fields['infrastructure_stables'] = new CheckBox();
+		$this->fields['infrastructure_greenhouse'] = new CheckBox();
+		$this->fields['infrastructure_goats'] = new CheckBox();
+		$this->fields['infrastructure_sheep'] = new CheckBox();
+		$this->fields['infrastructure_horses'] = new CheckBox();
+		
+		// Type of agriculture
+		$this->fields['horticulture'] = new CheckBox();
+		$this->fields['livestock_cattle_beef'] = new CheckBox();
+		$this->fields['livestock_cattle_dairy'] = new CheckBox();
+		$this->fields['livestock_poultry'] = new CheckBox();
+		$this->fields['livestock_hogs'] = new CheckBox();
+		$this->fields['livestock_goats'] = new CheckBox();
+		$this->fields['livestock_sheep'] = new CheckBox();
+		$this->fields['livestock_horses'] = new CheckBox();
+		$this->fields['aquaculture'] = new CheckBox();
+		$this->fields['tobacco'] = new CheckBox();
+		$this->fields['rowcrops'] = new CheckBox();
+
+		// Terms extendend 
+		$this->fields['lease_agreement'] = new TextField();
+		$this->fields['purchase_agreement'] = new TextField();
+
+		// Equipment
+		$this->fields['equipment'] = new CheckBox();
+		$this->fields['equipment_other'] = new CheckBox();
+		
+		// Misc
+		$this->fields['irrigation'] = new CheckBox();
+		$this->fields['goals'] = new TextField();
 
 
 }
