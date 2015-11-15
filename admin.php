ffc4fe0392abd4ee3722f3c00c1945561c2ec0d3
unknown <Kassiey@STU-KASSIEY7.berea.edu>
2015-11-15 10:13:39 -0500

    Created admin.php controller. Modified file names to make them easy to identify. Working on admin_template.php and admin_model.php

---
+++ admin.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$page_title = "Admin page";
+$panel_heading = "Admin";
+$page_body = "admin_template.php";
+
+
+include "templates/template.php";
+
+?>
---
+++ templates/admin_template.php
@@ -0,0 +1,26 @@
+
+	<form action = "same_page.php" method = "post">
+	<div class="row">
+		<div class="col-sm-2"><h3>Filter by:</h3></div>
+		<div class="col-sm-6"
+		<label class="checkbox-inline"><input type="checkbox" value="">Lease</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Buy/Sell</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Intern</label><br>
+		<label class="checkbox-inline"><input type="checkbox" value="">Vegetable</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Livestock</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Aquaculture</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Tobacco</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Row Crops</label><br>
+		<label class="checkbox-inline"><input type="checkbox" value="">Northern</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Southern</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Eastern</label>
+		<label class="checkbox-inline"><input type="checkbox" value="">Western</label>
+		</div>
+		<div class="col-sm-4"><button type="submit" class="btn btn-primary btn-md">Filter</button></div>
+	</div>
+	</form><br><br>
+	
+	<center><div class="row"></center>
+		<div class="col-sm-6">Farmers</div>
+		<div class="col-sm-6">Land Owners</div>
+	</div>
