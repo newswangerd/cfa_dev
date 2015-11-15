ba4e4e861ebdb5c1c3802f94679f2b17a04fe7ef
Yhenew <kassiey@berea.edu>
2015-11-15 12:55:46 -0500

    Added a login button at the top

--- templates/template.php
+++ templates/template.php
@@ -6,7 +6,13 @@
  <body>
 
  <div class="container">
-  	<center><img src = "templates/CFAlogos-06med.jpg"></center>
+ <div class="row">
+		<center><div class="col-md-10">
+			<img src = "templates/CFAlogos-06med.jpg">
+		</div></center>
+		<div class="col-md-2"><br><br><br><br>
+			<button type="button" class="btn btn-info btn-lg col-md-offset-1 " >Login</button></div>   
+</div>
 	 <div class="row">
 		 <div class="panel panel-primary">
 			 <div class="panel-heading"><?php echo $panel_heading ?></div>
