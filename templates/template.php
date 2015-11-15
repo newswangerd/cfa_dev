1cec5e43c97517a659e656e95a9652829c96be02
Yhenew <kassiey@berea.edu>
2015-11-15 15:12:49 -0500

    modified login button

--- templates/template.php
+++ templates/template.php
@@ -7,11 +7,11 @@
 
  <div class="container">
  <div class="row">
-		<center><div class="col-md-10">
+		<center><div class="col-md-9">
 			<img src = "templates/CFAlogos-06med.jpg">
 		</div></center>
-		<div class="col-md-2"><br><br><br><br>
-			<button type="button" class="btn btn-info btn-lg col-md-offset-1 " >Login</button></div>   
+		<div class="col-md-3"><br><br><br><br><button type="button" class="btn btn-warning"><h5>Registered already?</h5></button>
+			<button type="button" class="btn btn-warning btn-lg " >Login</button></div>   
 </div>
 	 <div class="row">
 		 <div class="panel panel-primary">
