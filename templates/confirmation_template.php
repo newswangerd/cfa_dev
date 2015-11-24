d72b75ec9fe87b349679d029c6e9f2b6d0764e80
Yhenew <kassiey@berea.edu>
2015-11-23 22:00:25 -0500

    feedback linked

--- templates/confirmation_template.php
+++ templates/confirmation_template.php
@@ -6,7 +6,11 @@
 		<p><b>Thank you for registering your information with Kentucky Community Farm Alliance.
 			Once your application is reviewed and a much has been found, we will contacut you.</b> </p>
       </div>
-	  <br>
+	  <div class="container">
+		<p>Please provide us your 
+			<a href="https://docs.google.com/forms/d/1q5V7bhpi3e3njORD4S3bMo-DN1TYZdv05qIfS4gMdwg/viewform?c=0&w=1">
+				<b>feedback.<b></a></p>
+	  </div>
 	  <div class="container">
 			<a href="index.php" class="btn btn-danger btn-sm" role="button">Logout</a>		  
 		</div>
